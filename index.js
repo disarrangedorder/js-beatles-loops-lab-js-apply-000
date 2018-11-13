7edc2f31a97f83c0ed55c5e2d060c9f62c732c92
Claire Nguyen <disarrangedorder@gmail.com>
2018-11-13 04:29:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,23 +7,20 @@ function theBeatlesPlay (musicians, instruments){
   return array
 }
 
-const facts = [
-"He was the last Beatle to learn to drive",
-"He was never a vegetarian",
-"He was a choir boy and boy scout",
-"He hated the sound of his own voice"
-];
-
 function johnLennonFacts(facts){
-
   var withExclamation = []
   let i = 0
-
   while (i < facts.length) {
     withExclamation.push(`${facts[i]}!!!`)
     i++
   }
-
   return withExclamation
+}
 
+function iLoveTheBeatles(num){
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    num++
+  } while (num < 15);
 }
