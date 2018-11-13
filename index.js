04125624ea7b8baa3509ae18fb102885aeaa064f
Claire Nguyen <disarrangedorder@gmail.com>
2018-11-13 04:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ function theBeatlesPlay (musicians, instruments){
     }
   return array
 }
+
+
+
+function johnLennonFacts(facts){
+
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+
+  var withExclamation = []
+  let i = 0
+  
+  while (i < facts.length) {
+    withExclamation.push(`facts[i]!!!`)
+    i++
+  }
+
+  return withExclamation
+  
+}
